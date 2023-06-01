a8addc4251b17ef5ed5611cd85a34357d18c12d1
rizzco1976 <franco_rizzuti@hotmail.com>
2023-06-01 01:27:40 -0300

    Practico N0 finalizado

---
+++ Practico0/promedio3.js
@@ -0,0 +1,18 @@
+let promedio1 = 5 + 7 + 9
+
+let promedio2 = -2 + -3 + -8
+
+let promedio3 = -1.2 + 5.3 + -3.3
+
+console.log(promedio1/3)
+console.log(promedio2/3)
+console.log(promedio3/3)
+
+//¿Cuál es el promedio entre 5, 7 y 9?
+// el resultado es 7
+
+//¿Cuál es el promedio entre -2 , -3, -8?
+// el resultado es -4.333333333333333
+
+//¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+// el resultado es 0.2666666666666666
