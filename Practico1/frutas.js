b2444326498342faeae1bf0fe4bff8f0564c0ba4
rizzco1976 <franco_rizzuti@hotmail.com>
2023-06-13 17:37:17 -0300

    agrega fruta.js

---
+++ Practico1/frutas.js
@@ -0,0 +1,14 @@
+//a. 
+let frutas = [];
+
+//b.
+
+frutas = ["banana", "frutilla", "manzana", "kiwi", "naranja"];
+
+//c.
+let ultima = frutas[frutas.length - 1];
+console.log("La ultima fruta agregada fue: "+ultima);
+
+//d.
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: "+primera);
